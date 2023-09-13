cfeeaea722251f3c75793577968a8d9b5504587e
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-09-13 13:35:35 -0300

    1

--- tcs/src/main/java/go/party/tcs/Controller.java
+++ tcs/src/main/java/go/party/tcs/Controller.java
@@ -1,6 +1,5 @@
 package go.party.tcs;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
