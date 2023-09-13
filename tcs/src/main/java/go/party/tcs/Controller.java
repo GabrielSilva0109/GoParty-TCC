b1c078214f092755482fd7093a611fda16fbec31
Pedro Aluisio <pedrogrando6@gmail.com>
2023-09-13 19:16:16 -0300

    primeiro commit

---
+++ tcs/src/main/java/go/party/tcs/Controller.java
@@ -0,0 +1,12 @@
+package go.party.tcs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    
+    @GetMapping("/home")
+    public String homePage(){
+        return "home";
+    }
+}
---
+++ tcs/src/main/java/go/party/tcs/TcsApplication.java
@@ -0,0 +1,13 @@
+package go.party.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsApplication.class, args);
+	}
+
+}
