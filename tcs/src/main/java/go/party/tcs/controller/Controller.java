790a70e81071af08dceccff2d3381867815b024b
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-09-15 09:20:45 -0300

    1

--- tcs/src/main/java/go/party/tcs/controller/Controller.java
+++ tcs/src/main/java/go/party/tcs/controller/Controller.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
     
-    @GetMapping("/home")
+    @GetMapping("/login")
     public String homePage(){
-        return "home";
+        return "login";
     }
 
     @GetMapping("/")
     public String redirectToHomePage() {
-        return "redirect:/home";
+        return "redirect:/login";
     }
 
     
