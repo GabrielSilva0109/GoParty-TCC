d0335e90bc64e50a8cf05eaff2ac0e46e3aebe8d
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-09-25 12:14:27 -0300

    1

--- tcs/src/main/java/go/party/tcs/controller/Controller.java
+++ tcs/src/main/java/go/party/tcs/controller/Controller.java
@@ -1,8 +1,14 @@
 package go.party.tcs.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import go.party.tcs.model.Usuario;
+import go.party.tcs.repository.UsuarioRepository;
 import go.party.tcs.service.UsuarioService;
 import jakarta.servlet.http.HttpSession;
 
@@ -20,7 +26,24 @@ public class Controller {
         return "redirect:/login";
     }
 
-  
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/perfil/{id}")
+    public String exibirPerfil(@PathVariable Integer id, Model model) {
+        // Buscar o usuário com o ID especificado no banco de dados
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            // Adicionar o usuário ao modelo para que ele possa ser exibido na página de perfil
+            model.addAttribute("usuario", usuario);
+            return "perfilUsuarios"; // Isso renderizará a página de perfil
+        } else {
+            // Lide com o caso em que o usuário não foi encontrado
+            return "redirect:/usuarios"; // Redirecione para uma página de lista de usuários, por exemplo
+        }
+    }
 
     
     
