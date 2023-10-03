aa8875a9957c410755b10798173192c4f9e81a3b
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-10-03 19:32:53 -0300

    Update Comentario.java

--- tcs/src/main/java/go/party/tcs/model/Comentario.java
+++ tcs/src/main/java/go/party/tcs/model/Comentario.java
@@ -14,8 +14,46 @@ public class Comentario {
     @Column(name = "id")
     private Integer id;
 
+    @Column(name = "texto")
+    private String texto;
+
     @ManyToOne
     @JoinColumn(name = "autor_id")
     private Usuario autor;
 
+    @ManyToOne
+    @JoinColumn(name = "evento_id")
+    private Evento evento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
 }
