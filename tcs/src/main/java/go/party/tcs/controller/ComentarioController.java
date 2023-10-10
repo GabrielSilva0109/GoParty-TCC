3eb712bba44aa432015dc90eb424dc51572b8805
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-10-09 21:30:05 -0300

    1

--- tcs/src/main/java/go/party/tcs/controller/ComentarioController.java
+++ tcs/src/main/java/go/party/tcs/controller/ComentarioController.java
@@ -1,11 +1,8 @@
 package go.party.tcs.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,7 +26,8 @@ public class ComentarioController {
     @PostMapping("/evento/{eventoId}/comentar")
     public String criarComentario(@PathVariable Integer eventoId,
         Comentario comentario,
-        HttpSession session
+        HttpSession session,
+        Model model
     ) {
         // Recupere o usuário da sessão
         Usuario usuario = (Usuario) session.getAttribute("usuario");
@@ -43,21 +41,10 @@ public class ComentarioController {
         
         // Salve o comentário no banco de dados (você deve implementar esse método em seu serviço)
         comentarioService.save(comentario); // Substitua comentarioService pelo serviço apropriado
-        
+        model.addAttribute("sessionUsuario", usuario);
         // Redirecione de volta para a página do evento
-        return "redirect:/perfil";
+        return "redirect:/home";
 
-    }
-
-    @GetMapping("/evento/{eventoId}/comentarios")
-    public String exibirComentarios(@PathVariable Integer eventoId, Model model) {
-        // Recupere os comentários associados ao evento pelo eventoId
-        List<Comentario> comentarios = comentarioService.encontrarComentariosPorEvento(eventoId); // Substitua comentarioService pelo serviço apropriado
-
-        // Adicione os comentários ao modelo para serem exibidos na página
-        model.addAttribute("comentarios", comentarios);
-
-        return "evento"; 
     }
 
 
--- tcs/src/main/java/go/party/tcs/controller/UsuarioController.java
+++ tcs/src/main/java/go/party/tcs/controller/UsuarioController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
