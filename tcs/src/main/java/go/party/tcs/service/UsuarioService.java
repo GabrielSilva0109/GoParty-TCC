db07e43f22457b79eb210933d342e639bea67af7
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-10-20 10:38:34 -0300

    1

--- tcs/src/main/java/go/party/tcs/service/UsuarioService.java
+++ tcs/src/main/java/go/party/tcs/service/UsuarioService.java
@@ -110,11 +110,6 @@ public class UsuarioService {
             followerRepository.delete(existingFollower);
         }
     }
-
-    public List<Follower> obterSeguidores(Integer userId) {
-        // Aqui, você deve chamar o método do repositório para buscar os seguidores com base no ID do usuário.
-        return followerRepository.findByFollowerId(userId);
-    }
     
 
     //OBTER NUMEROS DE SEGUIDORES 
