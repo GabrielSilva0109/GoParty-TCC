70e868fa0f765f924fc12391d0b3fe4070d00404
Pedro Aluisio <pedrogrando6@gmail.com>
2023-10-22 02:55:45 -0300

    NavBarLateral

---
+++ tcs/src/main/resources/static/css/js/barraLateral.js
@@ -0,0 +1,21 @@
+
+// ADICIONAR ATIVO AO CLIQUE
+var menuItem = document.querySelectorAll('.item-menu');
+
+function selectLink(){
+    menuItem.forEach((item) =>
+       item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+ item.addEventListener('click', selectLink)
+)
+
+var btnExp = document.querySelector('#btn-exp');
+var navBar = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    navBar.classList.toggle('expandir')
+})
