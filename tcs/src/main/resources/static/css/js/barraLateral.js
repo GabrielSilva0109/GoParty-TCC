73319823d715f2210c2c2b7fba842c0c92d91834
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2023-10-25 20:25:35 -0300

    Teste

--- tcs/src/main/resources/static/css/js/barraLateral.js
+++ tcs/src/main/resources/static/css/js/barraLateral.js
@@ -20,6 +20,33 @@ btnExp.addEventListener('click', function(){
     navBar.classList.toggle('expandir')
 })
 
+document.addEventListener('DOMContentLoaded', function() {
+    const btnNotificacoes = document.getElementById('modal-notificacoes');
+
+    btnNotificacoes.addEventListener('click', function() {
+        // Realize a requisição AJAX para a rota /notificacoes no servidor
+        fetch('/notificacoes', {
+            method: 'GET'
+            // Adicione outras opções de requisição, como cabeçalhos, conforme necessário
+        })
+        .then(response => {
+            // Lógica para lidar com a resposta do servidor
+            if (response.status === 200) {
+                // A resposta foi bem-sucedida (status 200)
+                // Você pode adicionar aqui a lógica para processar a resposta, se necessário.
+            } else {
+                // Lidar com possíveis erros de resposta
+                console.error('Erro na requisição: ' + response.statusText);
+            }
+        })
+        .catch(error => {
+            // Lidar com erros de rede ou outras exceções
+            console.error('Erro na requisição: ' + error);
+        });
+    });
+});
+
+
 
    
   
