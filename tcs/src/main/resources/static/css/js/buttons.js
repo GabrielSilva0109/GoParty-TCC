c5f9ff0dc4a06301fc3191e9a3e55b5fd5bb1d06
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2023-10-30 16:46:53 -0300

    Novo botão de curtidas

--- tcs/src/main/resources/static/css/js/buttons.js
+++ tcs/src/main/resources/static/css/js/buttons.js
@@ -70,4 +70,75 @@ function abrirExcluir() {
             }
 }
 
+//Requisição de curtida 
+
+// JavaScript
+function checkboxChanged(checkbox) {
+    const eventoId = checkbox.getAttribute('data-evento-id');
+    if (checkbox.checked) {
+        curtirEvento(eventoId);
+        
+    } else {
+        descurtirEvento(eventoId);
+    }
+}
+
+// JavaScript
+function curtirEvento(eventoId) {
+    const url = `/curtirEvento/${eventoId}`;
+
+    // O objeto de opções da solicitação, incluindo o método POST
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Se você estiver enviando dados no corpo da solicitação
+        },
+        // Adicione um corpo se estiver enviando dados
+        // body: JSON.stringify({ chave: valor }),
+    };
+
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            // Trate a resposta aqui, se necessário
+            console.log('Evento curtido com sucesso');
+            
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+}
+
+function descurtirEvento(eventoId) {
+    const url = `/descurtiEvento/${eventoId}`;
+
+    // O objeto de opções da solicitação, incluindo o método POST
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Se você estiver enviando dados no corpo da solicitação
+        },
+        // Adicione um corpo se estiver enviando dados
+        // body: JSON.stringify({ chave: valor }),
+    };
+
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            // Trate a resposta aqui, se necessário
+            console.log('Evento descurtido com sucesso');
+            
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+}
+
+
+
+
 
