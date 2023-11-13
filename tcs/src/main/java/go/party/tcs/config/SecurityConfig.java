3ca6902e096a3a4dc4f77c3d10c93ba608ee7564
GabrielSilva0109 <silvagabriel0109@gmail.com>
2023-11-13 10:57:23 -0300

    1

--- tcs/src/main/java/go/party/tcs/config/SecurityConfig.java
+++ tcs/src/main/java/go/party/tcs/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package go.party.tcs.config;
 
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,9 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    
+
+     
 
 }
